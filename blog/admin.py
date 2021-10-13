97df5f1eb154278e3c227b2eea0663a4fe10fbd0
Masoud <masoudhashemian1997@gmail.com>
2021-10-13 10:55:26 +0330

    thumbnail tag in admin

--- blog/admin.py
+++ blog/admin.py
@@ -35,7 +35,7 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("title" ,"slug" , "jpublish" ,"status",'category_to_str')
+    list_display = ("title","thumbnail_tag" ,"slug" , "jpublish" ,"status",'category_to_str')
     list_filter = ('publish', 'status')
     search_fields = ('title','description')
     prepopulated_fields = {'slug':('title',)}
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import format_html
 from django.utils import timezone
 from extensions.utils import jalali_converter
 
@@ -66,5 +67,9 @@ class Article(models.Model):
 
     def category_published(self):
         return self.category.filter(status=True)
+
+    def thumbnail_tag(self):
+        return format_html("<img width=100 height=70 style='border-radius: 5px;' src='{}'>".format(self.thumbnail.url))
+    thumbnail_tag.short_description = "عکس"
 
     objects = ArticleManager()
