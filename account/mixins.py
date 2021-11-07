ce423567d62a944e078c059d7ecc48e15226a8d9
Masoud <masoudhashemian1997@gmail.com>
2021-11-07 09:38:51 +0330

    52

--- account/mixins.py
+++ account/mixins.py
@@ -1,5 +1,6 @@
 from django.http import Http404
-
+from django.shortcuts import get_object_or_404
+from blog.models import Article
 
 class FieldsMixin():
     def dispatch(self, request, *args, **kwargs):
@@ -37,3 +38,13 @@ class FormValidMixin():
             self.obj.author = self.request.user
             self.obj.status = 'd'
         return super().form_valid(form)
+
+class AuthorAccessMixin():
+    def dispatch(self, request,pk, *args, **kwargs):
+        article = get_object_or_404(Article,pk=pk)
+        if article.author == request.user and article.status == 'd' or request.user.is_superuser:
+            return super().dispatch(request, *args, **kwargs)
+            
+        else:
+            raise Http404("you can't see this page")
+
