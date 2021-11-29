8f5402d27ee9449d28a7d0c3704d9b5de4d10783
masoudhmn <masoudhashemian1997@gmail.com>
2021-11-29 10:14:51 +0330

    crate profile view 059

--- account/views.py
+++ account/views.py
@@ -2,26 +2,28 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import User
 from .mixins import (
     FieldsMixin,
     FormValidMixin,
     AuthorAccessMixin,
     SuperUserAccessMixin
-    )
+)
 from django.views.generic import (
-ListView , 
-CreateView , 
-UpdateView,
-DeleteView
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView
 )
 from blog.models import Article
 
 # Create your views here .
-# @login_required 
+# @login_required
 # def home(request):
 #     return render(request,'registration/home.html')
 
-class ArticleList(LoginRequiredMixin,ListView):
+
+class ArticleList(LoginRequiredMixin, ListView):
     # queryset = Article.objects.all()
     template_name = "registration/home.html"
 
@@ -29,18 +31,31 @@ class ArticleList(LoginRequiredMixin,ListView):
         if self.request.user.is_superuser:
             return Article.objects.all()
         else:
-            return Article.objects.filter(author = self.request.user)
+            return Article.objects.filter(author=self.request.user)
 
-class ArticleCreate(LoginRequiredMixin,FormValidMixin,FieldsMixin,CreateView):
-    model = Article 
+
+class ArticleCreate(LoginRequiredMixin, FormValidMixin, FieldsMixin, CreateView):
+    model = Article
     template_name = "registration/article-create-update.html"
 
 
-class ArticleUpdate(AuthorAccessMixin,FormValidMixin,FieldsMixin,UpdateView):
-    model = Article 
+class ArticleUpdate(AuthorAccessMixin, FormValidMixin, FieldsMixin, UpdateView):
+    model = Article
     template_name = "registration/article-create-update.html"
 
-class ArticleDelete(SuperUserAccessMixin,DeleteView):
+
+class ArticleDelete(SuperUserAccessMixin, DeleteView):
     model = Article
     success_url = reverse_lazy('account:home')
     template_name = "registration/article_confirm_delete.html"
+
+
+class Profile(UpdateView):
+    model = User
+    template_name = "registration/profile.html"
+    fields = ['username', 'email', 'first_name', 'last_name',
+              'special_user', 'is_author']
+    success_url = reverse_lazy("account:profile")
+
+    def get_object(self):
+        return User.objects.get(pk = self.request.user.pk)
